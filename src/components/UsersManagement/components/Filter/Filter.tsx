57860e5cd7d44bd5c87b65fccaf5b1f3874a5ab8
Deividas Šaltupis <deividassaltupis18@gmail.com>
2021-12-09 00:30:15 +0200

    Reused and wrong emails tabs fixed

--- src/components/UsersManagement/components/Filter/Filter.tsx
+++ src/components/UsersManagement/components/Filter/Filter.tsx
@@ -1,7 +1,10 @@
 import { FC } from 'react';
-import { Routes } from "~/constants";
-import { IItem } from "~/services/getUserItems";
-import FilterTab from "./components/FilterTab"
+import { Routes } from '~/constants';
+import { IItem } from '~/services/getUserItems';
+import FilterTab from './components/FilterTab';
+
+import userHasWrongEmail from '~/utils/userHasWrongEmail';
+import userHasReusedEmail from '~/utils/userHasReusedEmail';
 
 import './filter-style.scss';
 
@@ -9,20 +12,30 @@ interface IFilter {
   items: Array<IItem>;
 }
 
-const Filter: FC<IFilter> = ({items}) => {
-  const weakItemsCount = items.reduce((count, item) => (
-     (count + 1) 
-  ), 0)
+const Filter: FC<IFilter> = ({ items }) => {
+  const wrongEmailsCount = items.reduce((count, item) => {
+    if (userHasWrongEmail(item)) count++;
+    return count;
+  }, 0);
 
-  const reusedItemsCount = items.reduce((count, item) => (
-    (count + 1)
-  ), 0)
+  const reusedEmailsCount = items.reduce((count, item) => {
+    if (userHasReusedEmail(item, items)) count++;
+    return count;
+  }, 0);
 
   return (
     <div className="filter">
-      <FilterTab title="all" count={items.length} path={Routes.Users}/>
-      <FilterTab title="Wrong" count={weakItemsCount} path={Routes.Weak}/>
-      <FilterTab title="Reused" count={reusedItemsCount} path={Routes.Reused}/>
+      <FilterTab title="All" count={items.length} path={Routes.Users} />
+      <FilterTab
+        title="Wrong"
+        count={wrongEmailsCount}
+        path={Routes.WrongEmails}
+      />
+      <FilterTab
+        title="Reused"
+        count={reusedEmailsCount}
+        path={Routes.ReusedEmails}
+      />
     </div>
   );
 };
